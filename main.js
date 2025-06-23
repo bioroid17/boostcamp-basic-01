206f718b1e7d64f250f4d7795a0cc153b0e52a84
bioroid17 <trace1997@ajou.ac.kr>
2025-06-23 18:17:53 +0900

    snake, ladder 분리

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-function nextPosition(current, dice) {
-  const next = current + dice;
+// 사다리칸 분리
+function ladder(next, dice) {
   if (next == 4) {
     return dice + 10;
   } else if (next == 8) {
@@ -7,13 +7,44 @@ function nextPosition(current, dice) {
   } else if (next == 28) {
     return dice + 48;
   } else if (next == 21) {
-    return dice + 42;
+    return dice + 21; // 42가 아니라 21을 더해야 한다. 21 -> 42이기 때문
   } else if (next == 50) {
     return dice + 17;
   } else if (next == 71) {
-    return dice + 92;
+    return dice + 21; // 92가 아니라 21을 더해야 한다. 71 -> 92이기 때문
   } else if (next == 80) {
     return dice + 19;
+  }
+}
+
+// 뱀칸 분리
+function snake(next, dice) {
+  if (next == 97) {
+    return dice - 19;
+  } else if (next == 95) {
+    return dice - 39;
+  } else if (next == 88) {
+    return dice - 64;
+  } else if (next == 62) {
+    return dice - 44;
+  } else if (next == 48) {
+    return dice - 22;
+  } else if (next == 36) {
+    return dice - 30;
+  } else if (next == 32) {
+    return dice - 22;
+  }
+}
+
+function nextPosition(current, dice) {
+  const next = current + dice;
+
+  const ladderStarts = [4, 8, 21, 28, 50, 71, 80]; // 사다리 시작 위치
+  const snakeStarts = [32, 36, 48, 62, 88, 95, 97]; // 뱀 시작 위치
+  if (ladderStarts.includes(next)) {
+    return ladder(next, dice);
+  } else if (snakeStarts.includes(next)) {
+    return snake(next, dice);
   }
 
   return dice;
